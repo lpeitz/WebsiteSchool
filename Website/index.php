876d72770a83c1c54fbd7e076f779c2dec90e6db
lpeitz <peitzlouis3gmail.com>
2024-04-24 18:37:51 +0200

    IDKMaybeNew

---
+++ Website/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Website/Style/stlye.css">
+    <script type="text/javascript" src="/Website/JS/clock.js"></script>
+    <title>Home</title>
+</head>
+<body>
+    
+        <div class="nav">
+            <div class="logo">
+                <span class="image">
+                    <img src="/Website/logo.png" alt="logo">
+                </span>
+            </div>
+
+            <div class="right-links">
+                <a href="#">Change Profile</a>
+                <a href="logout.php"> <button class="btn">Log out</button></a>
+            </div>
+        </div>
+
+        <main>
+            <div class="main-box top">
+                <div class="top">
+                  <div class="box">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus provident architecto quam voluptates maxime. Libero quae quas sit sed iusto. Minus, non. Architecto, perferendis. Obcaecati officiis ipsam libero eaque sapiente!</p>
+                  </div>
+                  <div class="box">
+                      <p>Your email is Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quae nulla cum amet quibusdam voluptas dolorum sint, eos voluptates eligendi deleniti facere nostrum illum repudiandae, laboriosam ratione placeat reprehenderit obcaecati.</p>
+                  </div>
+                </div>
+                <div class="bottom">
+                  <div class="box">
+                      <p>12:30, 01.09.1939</p> 
+                  </div>
+                </div>
+             </div>
+        </main>
+</body>
+</html>
